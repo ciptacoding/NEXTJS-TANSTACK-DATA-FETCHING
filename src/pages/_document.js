94a255d2c7b780966daf343f29d4b498baac8ee4
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-10-13 20:22:18 +0800

    Feat: initial color mode script

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+import { ColorModeScript } from "@chakra-ui/react";
+import theme from "@/pages/theme";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class Document extends NextDocument {
+   render() {
+      return (
+         <Html lang="en">
+            <Head />
+            <body>
+               <ColorModeScript
+                  initialColorMode={theme.config.initialColorMode}
+               />
+               <Main />
+               <NextScript />
+            </body>
+         </Html>
+      );
+   }
 }
